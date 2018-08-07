ca6c8315c7021889c2e793441ee7ca6625b622bf
tngoon <tricia.ngoon@gmail.com>
2018-08-07 10:34:57 -0700

    added files for scaffold website

---
+++ js/portfolio.js
@@ -0,0 +1,14 @@
+'use strict';
+
+// Call this function when the page loads (the "ready" event)
+$(document).ready(function() {
+	initializePage();
+})
+
+/*
+ * Function that is called when the document is ready.
+ */
+function initializePage() {
+	// add any functionality and listeners you want here
+	console.log("Javascript is connected!");
+}
